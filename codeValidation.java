8ad838b9008830df9bc209c8f955fd4f1eff119d
dharmesh1212 <dharmeshpatel6367@gmail.com>
2021-09-13 13:07:38 +0530

    first codecodeValidation.java

---
+++ codeValidation.java
@@ -0,0 +1,68 @@
+package practice;
+import java.util.regex.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner; 
+public class codeValidation {
+	static Scanner sc= new Scanner(System.in);
+	public static void main(String[] args) {
+	
+		 boolean IsDateValid=false;
+		 String DOJ;
+
+		 System.out.println("ENTER THE DATE= DD-MM-YYYY");
+		 DOJ=sc.nextLine();
+		 Check(DOJ);
+		 
+		 
+		 
+		 
+		 
+	
+				
+				
+				//	 Date date1= sc.next();
+				  
+				 
+//				 System.out.println("Enter Employee ID=");
+					
+//				String emp=sc.nextLine();
+//				 emp=emp.toUpperCase();
+//			    emp=emp.trim(); 
+//			    emp1=emp.substring(3);
+//			    
+//				if(emp.startsWith("EMP") && emp1.matches("[0-9]{3}")) {  
+//					
+//				 System.out.println("employee id is= "+emp);
+//				 IsValid=true;
+//				 } 
+				
+//				
+//			else {
+//				System.out.println("invalid input enter again");
+//				IsDateValid=false;
+//			} 
+			}
+		 
+	
+	public static void Check(String s) {
+		String DOJ;
+	    boolean IsDateValid=false;
+	   while(!IsDateValid) {
+		  
+		 
+			if(s.length()<=10 && s.charAt(2)=='-' && s.charAt(5)=='-' && s.matches("[--/[0-9]]{10}")) {
+			 		
+				 System.out.println("DATE -  "+ s);
+				IsDateValid=true;
+			}
+			 
+			else {
+				System.out.println("Invalid Format Please Enter Agian");
+				IsDateValid=false;
+			}
+	
+	 }
+		  
+	}
+}

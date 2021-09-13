1f579d12d6a2ce8de704113ed8ca6350c89ede48
dharmesh1212 <dharmeshpatel6367@gmail.com>
2021-09-13 14:56:25 +0530

    first codetestpractice.java

---
+++ testpractice.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class testpractice {
+
+	public static void main(String[] args) {
+		 
+		Scanner sc= new Scanner(System.in);
+		
+		
+
+		
+		 
+}
+
+}
